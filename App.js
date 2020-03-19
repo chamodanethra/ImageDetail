4ced1852728ab96282b2325fafecd58ba6256ec1
chamodanethra <chamoda@aeonicdesk.com>
2020-03-19 11:26:03 +0530

    Android working static

--- App.js
+++ App.js
@@ -7,24 +7,49 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, NativeModules, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+  Button,
+  Platform,
+} from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {boundaryCoordinates: {}};
+    this.turnOn();
   }
   turnOn = () => {
-    NativeModules.ImageDetail.turnOn(
-      `http://www.cf.wmich.edu/images/ClipArt/LightBulb3.png`,
-    );
-    this.updateStatus();
+    if (Platform.OS == 'ios') {
+      NativeModules.ImageDetail.turnOn(
+        `https://la-frontend.s3-ap-southeast-1.amazonaws.com/images/icons/fruits/vegetable-3d-tomatto.png`,
+      );
+      this.updateStatus();
+    } else if (Platform.OS == 'android') {
+      let object = 'vegetable-3d-tomatto';
+      NativeModules.ImageDetail.turnOn(
+        `https://la-frontend.s3-ap-southeast-1.amazonaws.com/images/icons/fruits/${object}.png`,
+      )
+        .then(boundaryCoordinates => {
+          // console.log(boundaryCoordinates.startX);
+          // console.log(boundaryCoordinates.startY);
+          // console.log(boundaryCoordinates.endX);
+          // console.log(boundaryCoordinates.endY);
+          this.setState({boundaryCoordinates});
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   };
 
   updateStatus() {
     NativeModules.ImageDetail.getStatus((error, boundaryCoordinates) => {
       this.setState({boundaryCoordinates: boundaryCoordinates});
-      console.log('boundaryCoordinates ', this.state.boundaryCoordinates);
+      //console.log('boundaryCoordinates ', this.state.boundaryCoordinates);
     });
   }
 
@@ -32,8 +57,10 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Light App!!</Text>
-        <Text> Go</Text>
-        <Button onPress={this.turnOn} title="Turn ON " color="#FF6347" />
+        <Text> {this.state.boundaryCoordinates.startX}</Text>
+        <Text> {this.state.boundaryCoordinates.startY}</Text>
+        <Text> {this.state.boundaryCoordinates.endX}</Text>
+        <Text> {this.state.boundaryCoordinates.endY}</Text>
       </View>
     );
   }
--- android/app/src/main/java/com/imageprocessing/ImageDetail.java
+++ android/app/src/main/java/com/imageprocessing/ImageDetail.java
@@ -4,6 +4,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.util.Log;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,6 +14,7 @@ import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.uimanager.IllegalViewOperationException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
@@ -27,56 +29,59 @@ public class ImageDetail extends ReactContextBaseJavaModule  {
     public static WritableMap map = new WritableNativeMap();
 
     @ReactMethod
-    public void getStatus(Callback callback) {
-        //Log.d("test", String.valueOf(map));
-        callback.invoke(null, map);
-    }
+    public void turnOn(String url, Promise promise) {
+        Bitmap bmp = null;
+        try {
+            WritableNativeMap boundaryCoordinates = new WritableNativeMap();
+            bmp = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            int width = bmp.getWidth();
+            int height = bmp.getHeight();
 
-    @ReactMethod
-    public void turnOn(String url) throws IOException {
-        URL imageURL = new URL(url);
-        Bitmap bmp = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
+            int startX = width;
+            int startY = height;
+            int endX = 0;
+            int endY = 0;
 
-        int width = bmp.getWidth();
-        int height = bmp.getHeight();
-
-        int startX = width;
-        int startY = height;
-        int endX = 0;
-        int endY = 0;
-
-
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                boolean isTransparent = Color.alpha(bmp.getPixel(x, y)) == 0;
-                if (!isTransparent ){
-                    if (x > endX ){
-                        endX = x;
-                    }
-                    if (x < startX) {
-                        startX = x;
-                    }
-                    if (y > endY) {
-                        endY = y;
-                    }
-                    if (y < startY) {
-                        startY = y;
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    boolean isTransparent = Color.alpha(bmp.getPixel(x, y)) == 0;
+                    if (!isTransparent) {
+                        if (x > endX) {
+                            endX = x;
+                        }
+                        if (x < startX) {
+                            startX = x;
+                        }
+                        if (y > endY) {
+                            endY = y;
+                        }
+                        if (y < startY) {
+                            startY = y;
+                        }
                     }
                 }
             }
+            boundaryCoordinates.putInt("startX", startX);
+            boundaryCoordinates.putInt("startY", startY);
+            boundaryCoordinates.putInt("endX", endX);
+            boundaryCoordinates.putInt("endY", endY);
+
+            Log.d("TAG", "turnOn: " + startX);
+            promise.resolve(boundaryCoordinates);
+        } catch (IOException e) {
+            promise.reject(e);
         }
 
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("startX", startX);
-        hm.put("startY", startY);
-        hm.put("endX", endX);
-        hm.put("endY", endY);
+        // HashMap<String, Integer> hm = new HashMap<>();
+        // hm.put("startX", startX);
+        // hm.put("startY", startY);
+        // hm.put("endX", endX);
+        // hm.put("endY", endY);
+        //
+        // for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+        // map.putInt(entry.getKey(), entry.getValue());
+        // }
 
-        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
-            map.putInt(entry.getKey(), entry.getValue());
-        }
-
-        Log.d("id", "turnOn: ");
     }
     @Override
     public String getName() {
